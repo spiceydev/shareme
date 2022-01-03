27ddd3e0255efebea66598f4d1d182c7516fbf2a
spiceydev <spiceydev@gmail.com>
2022-01-03 14:35:42 +1300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './containers/Home';
+
+const App = () => (
+  <Routes>
+    <Route path="login" element={<Login />} />
+    <Route path="/*" element={<Home />} />
+  </Routes>
+);
+
+export default App;
